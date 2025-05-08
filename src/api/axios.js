40ebe2d990ee542ecff262f1e00425b8a5b73014
eliasjmorelos <ingeliasmorelos@gmail.com>
2025-05-08 11:57:14 -0500

    first deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // apunta al backend de Laravel
+    baseURL: 'https://api-decameron-hotel.onrender.com:8000/api', // apunta al backend de Laravel
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
